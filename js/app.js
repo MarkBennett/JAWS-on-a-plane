8c9a1c1be8368c82a9b7b6d0ee47ab3cebc569c0
Mark Bennett <mark@burmis.ca>
2012-01-27 14:52:33 -0700

    We can shoot now!

--- js/app.js
+++ js/app.js
@@ -4,15 +4,17 @@
 
   /** PlayState is the main state where players batter the sharks. */
   function PlayState() {
-    var player;
+    var player,
+        bullets = new jaws.SpriteList();
 
     this.setup = function() {
       player = jaws.Sprite({image: "assets/images/plane.png", x: 10, y: 100});
-      jaws.log("Player width = " + player.width + ", height = " + player.height);
+      player.can_shoot = true;
+
       jaws.on_keydown("esc", function() {
         jaws.switchGameState(MenuState);
       });
-      jaws.preventDefaultKeys(["left", "right", "up", "down"]);
+      jaws.preventDefaultKeys(["left", "right", "up", "down", "space"]);
     };
     this.update = function() {
       // Move the plane
@@ -22,13 +24,35 @@
       if(jaws.pressed("up")) { player.y -= 2; }
       forceInsideCanvas(player);
 
+      // Shoot
+      if(jaws.pressed("space")) {
+        if (player.can_shoot) {
+          player.can_shoot = false;
+          bullets.push(new Bullet(player.rect().right - 3, player.y + 5));
+          setTimeout(function() {
+            player.can_shoot = true;
+          }, 100);
+        }
+      }
+
+      bullets.deleteIf(jaws.isOutsideCanvas);
       fps.innerHTML = jaws.game_loop.fps;
     };
 
     this.draw = function() {
       jaws.context.clearRect(0, 0, jaws.width, jaws.height);
       player.draw();
+      bullets.draw();
     };
+
+    function Bullet(x, y) {
+      this.x = x
+      this.y = y
+      this.draw = function() {
+        this.x += 4
+        jaws.context.drawImage(jaws.assets.get("assets/images/bullet.png"), this.x, this.y)
+      }
+    }
   };
 
   /* MenuState is our lobby/welcome state where the gamer
@@ -53,11 +77,6 @@
     };
   };
 
-  window.onload = function() {
-    jaws.assets.add("assets/images/plane.png");
-    jaws.start(MenuState);
-  };
-
   /**
    * Force <b>item</b> inside canvas by setting items x/y parameters
    * <b>item</b> needs to have the properties x, y, width & height
@@ -68,5 +87,12 @@
     if(item.y < 0)              { item.y = 0 }
     if(item.y + item.height > jaws.height )    { item.y = jaws.height - item.height }
   }
+
+  window.onload = function() {
+    jaws.assets.add("assets/images/plane.png");
+    jaws.assets.add("assets/images/bullet.png");
+    jaws.start(MenuState);
+  };
+
 
 }());
